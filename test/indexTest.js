f78fd3eba2e3c9f6531f72b00d25a9666bf015db
DixonMch <dixonmogaka@gmail.com>
2023-03-12 21:40:51 +0300

    Completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,11 +1,11 @@
 require ( './root.js' );
 
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+describe('shout(string)', function () {
+    it('receives one argument and returns it in all caps', function shout() {
     expect(shout('hello')).toEqual('HELLO');
-  })
-})
+  });
+});
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
